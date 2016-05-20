eae629471a3053f71781b4613fdfe8cf622b95f6
lennyKim <heliotherapyy@gmail.com>
2016-05-20 17:28:45 +0900

    (fix) Erase space before 8888
    Users now can access to localhost:8888 by simply copying the address given by the terminal

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use(function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-    console.log("Express started on http://localhost: " +
+    console.log("Express started on http://localhost:" +
         app.get('port') + '; press Ctrl-C to terminate.');
 });
