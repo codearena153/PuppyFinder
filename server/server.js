e67d5093e385bdc7c9af8c94e10874cb50d4e5aa
lennyKim <heliotherapyy@gmail.com>
2016-05-24 22:22:21 +0900

    (feat) make query request to mongodb via admin/upload.html

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var path = require('path');
+var Puppy = require('./db/Puppy.model');
+var db = 'mongodb://localhost/puppy';
 
 var app = express();
 app.set('port', process.env.PORT || 8888);
 
-// so that broswer can locate index.html inside client folder
+// Connects mongo DB
+mongoose.connect(db);
+
+// Middleware loads
 app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/admin'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req, res) {
     res.type('text/plain');
@@ -26,11 +39,32 @@ app.get('/update', function(req, res) {
 app.get('/stat', function(req, res) {
     res.sendFile(__dirname + '/admin/stat.html');
 });
-// app.use(function(req, res) {
-//     res.type('text/plain');
-//     res.status(404);
-//     res.send('404 - Not Found');
-// });
+
+app.post('/result', function(req, res) {
+    var puppy = new Puppy();
+
+    // create a new db document
+    puppy.breed = req.body.breed;
+    puppy.description = req.body.description;
+    puppy.image = req.body.image;
+    puppy.isUserAllergic.absent = req.body.allergic;
+    puppy.isUserAbsent.absent = req.body.absent;
+    puppy.isUserActive.active = req.body.active;
+    puppy.isUserSingle.single = req.body.single;
+    puppy.isPuppyFriendly.friendly = req.body.friendly;
+    puppy.isPuppyInside.inside = req.body.inside;
+    puppy.initialCost.min = req.body.initial_min;
+    puppy.initialCost.max = req.body.initial_max;
+    puppy.maintenance.cost = req.body.maintenance;
+
+    puppy.save(function(err, puppy) {
+      if (err)  res.send("error saving new puppy");
+      else {
+        console.log(puppy);
+        res.send(puppy);
+      }
+    });
+});
 
 app.listen(app.get('port'), function() {
     console.log("Express started on http://localhost:" +
