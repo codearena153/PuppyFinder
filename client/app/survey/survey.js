18c71eb973e7f43c89dc723237112adfc537cb48
lennyKim <heliotherapyy@gmail.com>
2016-05-26 12:04:34 +0900

    (cleanup) delete console log statements for debugging

--- client/app/survey/survey.js
+++ client/app/survey/survey.js
@@ -2,7 +2,6 @@ angular.module('puppyfinder.survey', [])
 
 .controller('SurveyController', function ($scope, $window, $location, $http, QuestionList) {
     $scope.questions = QuestionList.questions;
-    console.log("Does QuestionList Factory work? ", QuestionList.questions);
 
     var survey = {
         allergic: "true",
