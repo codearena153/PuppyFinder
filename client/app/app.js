c302294b94a5fe763d93d653f5e207ac92231469
lennyKim <heliotherapyy@gmail.com>
2016-05-28 09:42:55 +0900

    (fix) remove periods for selecting opions

--- client/app/app.js
+++ client/app/app.js
@@ -41,8 +41,8 @@ angular.module('puppyfinder', [
             content: "평소엔 너무나 사랑스러운 반려견도 제대로 활동할 수 있는 환경을 만들어주지 못한다면 최고의 말썽꾸러기가 되기도 합니다. 반려견을 맞이하기 전에 생활하기에 알맞은 환경을가지고 있는지 고려해 주세요",
             name: "inside",
             options: [
-              { value: "true", text: "네, 마당이나 뒤뜰에 공간을 마련할 수 있어요." },
-              { value: "false", text: "아니오, 아늑한 실내에서 키울 거예요." }
+              { value: "true", text: "네, 마당이나 뒤뜰에 공간을 마련할 수 있어요" },
+              { value: "false", text: "아니오, 아늑한 실내에서 키울 거예요" }
             ]
         },
 
@@ -53,7 +53,7 @@ angular.module('puppyfinder', [
             content: "반려견을 혼자 돌보게 되면 함께 보내는 시간에 따라 반려견이 외로워할 수 있기 때문에 가급적이면 2마리를 함께 키우는 것이 좋습니다. 독신이시라면 반려견에 충분한 애정을 주실 수 있는지, 가족분들과 함께 살고 계신다면 다른 가족 분들이 반려견을 함께 돌봐주실 준비가 되어 있는지 생각해 주세요.",
             name: "single",
             options: [
-              { value: "false", text: "네, 가족들과 함께 살고 있어요." },
+              { value: "false", text: "네, 가족들과 함께 살고 있어요" },
               { value: "true", text: "아니요, 혼자 살고 있지만 충분한 애정과 관심을 줄 수 있어요!" }
             ]
         },
@@ -65,7 +65,7 @@ angular.module('puppyfinder', [
             content: "조깅 등 밖에서 활발하게 활동하는 걸 좋아하신다면 함께 뛰어다닐 수 있는 운동량이 많은 반려견을 만나셔야겠네요! 조용한 실내 활동을 더 좋아하신다면 차분한 반려견을 추천해 드릴게요.",
             name: "active",
             options: [
-              { value: "true", text: "네, 밖에 나가는 걸 좋아하고, 반려견과 함께 운동하고 싶어요." },
+              { value: "true", text: "네, 밖에 나가는 걸 좋아하고, 반려견과 함께 운동하고 싶어요" },
               { value: "false", text: "아니오, 집에서 조용히 쉬는 걸 좋아해요. 한다면 가끔 산책 정도?" }
             ]
         },
@@ -77,8 +77,8 @@ angular.module('puppyfinder', [
             content: "불가피하게 집을 자주 비우신다면 텅 빈 집을 지킬 수 있는 용감한 친구들이 좋겠죠? 그래도 역시 2마리를 함께 키우시는 게 가장 좋다는 점, 잊지 말아 주세요!",
             name: "absent",
             options: [
-              { value: "true", text: "네, 제가 없는 동안 반려견이 집을 잘 지켜줬으면 좋겠어요." },
-              { value: "false", text: "아니오, 집에서 반려견과 많은 시간을 함께 보낼 거예요." }
+              { value: "true", text: "네, 제가 없는 동안 반려견이 집을 잘 지켜줬으면 좋겠어요" },
+              { value: "false", text: "아니오, 집에서 반려견과 많은 시간을 함께 보낼 거예요" }
             ]
         },
 
@@ -89,8 +89,8 @@ angular.module('puppyfinder', [
             content: "혹은 가족 구성원 중에 그런 분이 있으신가요? 모든 개는 침과 각질에 단백질이 있고, 털이 빠져 어떤 사람에게는 알레르기 반응을 일으킬 수 있습니다. 하지만 어떤 개들은 털이 적게 빠지기도 하죠. 그 외에, 집 안에서 중요한 작업을 하시거나, 옷을 많이 아끼시는 분들도 털이 적게 날리는 종을 만나셔야 할 거예요!",
             name: "allergic",
             options: [
-              { value: "true", text: "네, 털이 적게 날리면 좋겠어요." },
-              { value: "false", text: "아니오, 제 기관지는 아주 튼튼해요. 청소는 자주 하면 되죠, 뭐." }
+              { value: "true", text: "네, 털이 적게 날리면 좋겠어요" },
+              { value: "false", text: "아니오, 제 기관지는 아주 튼튼해요. 청소는 자주 하면 되죠, 뭐" }
             ]
         },
 
@@ -102,8 +102,8 @@ angular.module('puppyfinder', [
             name: "friendly",
             options: [
               { value: "true", text: "활발하고 사교적인 성격의 반려견이 좋아요!" },
-              { value: "false", text: "조용하고 차분한 성격의 반려견이 좋겠어요." },
-              { value: "dafault", text: "상관없어요." },
+              { value: "false", text: "조용하고 차분한 성격의 반려견이 좋겠어요" },
+              { value: "dafault", text: "상관없어요" },
             ]
         },
 
