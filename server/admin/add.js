8ea35568a4f3e44e6b976184bf678809ec7dbcea
CHOSANG WOOK <chosangwook@CHOSANGui-MacBook-Pro.local>
2016-05-28 15:22:26 +0900

    (feat) [admin.html] apply bootstrap, change links into bootstrap anchor buttons (feat) [remove.html] apply bootstrap (feat) [update.html] apply bootstrap, correct selected option problem in select tag, successfully update (feat) [upload.html] apply bootstrap, apply angular(now pair with add.js) (feat) [update.js] update successfully, all cases(undefined, truncated) resolved (feat) [add.js] new angular JS, upload new puppy data, change upload data formatfrom form data to angular model data, so req.body format is changed (feat) [server.js] change app.post('/result') due to modified req.body format

---
+++ server/admin/add.js
@@ -0,0 +1,33 @@
+var app = angular.module("AddApp", []);
+
+app.controller("AddController", function($scope, $http, $window) {
+  $scope.allergic = [['true', '많음'], ['false', '적음']];
+  $scope.absent = [['true', '가능'], ['false', '불가능']];
+  $scope.active = [['true', '활동적'], ['false', '보통']];
+  $scope.single = [['true', '혼자 잘 지냄'], ['false', '외로움을 잘탐']];
+  $scope.friendly = [['true', '강함'], ['false', '약함'], ['default', '보통']];
+  $scope.inside = [['true', '실내'], ['false', '실외'], ['default', '상관없음']];
+
+  $scope.inputShow = true;
+  $scope.tableShow = false;
+
+  $scope.add = function(puppy) {
+    console.log("in add");
+    
+    $scope.inputShow = !$scope.inputShow;
+    $scope.tableShow = !$scope.tableShow;
+
+    $http.post("/result", puppy) 
+      .success(function(response) {
+        console.log("Just added: ", response);
+        $scope.puppy = response;
+        //$scope.puppy = response;
+      });  
+  }; 
+
+  $scope.goback = function() {
+    console.log("in goback function");
+    $window.location.href = '/admin'
+  }; 
+
+})
--- server/admin/update.js
+++ server/admin/update.js
@@ -2,14 +2,12 @@ var app = angular.module("UpdateApp", []);
 
 app.controller("UpdateController", function($scope, $http) {
   
-  $scope.allergic = [[true, '많음'], [false, '적음']];
-  $scope.absent = [[true, '가능'], [false, '불가능']];
-  $scope.active = [[true, '활동적'], [false, '보통']];
-  $scope.single = [[true, '혼자 잘 지냄'], [false, '외로움을 잘탐']];
-  $scope.friendly = [["true", '강함'], ["false", '약함'], ["default", '보통']];
+  $scope.allergic = [['true', '많음'], ['false', '적음']];
+  $scope.absent = [['true', '가능'], ['false', '불가능']];
+  $scope.active = [['true', '활동적'], ['false', '보통']];
+  $scope.single = [['true', '혼자 잘 지냄'], ['false', '외로움을 잘탐']];
+  $scope.friendly = [['true', '강함'], ['false', '약함'], ['default', '보통']];
   $scope.inside = [['true', '실내'], ['false', '실외'], ['default', '상관없음']];
-  //$scope.initialCost = [['p0-10', ' 10만원 이하'], ['p10-20', '10만원~20만원'], ['p20-50', '20만원~50만원'], ['p50-100', '50만원~100만원'], ['p100-500', '100만원~500만원'], ['p500-1000', '500만원~1000만원'], ['p1000', '1000만원 이상']];
-  //$scope.maintenance = [['c0-10', ' 10만원 이하'], ['c10-20', '10만원~20만원'], ['c20-30', '20만원~30만원'], ['c30-50', '30만원~50만원'], ['c50-100', '50만원~100만원'], ['c100', '100만원 이상']];
 
   $http.get("/puppies")
     .success(function(response) {
@@ -28,10 +26,11 @@ app.controller("UpdateController", function($scope, $http) {
   };
 
   $scope.update = function(puppy) {
+    console.log("in update: ", puppy._id);
     $http.put("/puppies/" + puppy._id, puppy) 
       .success(function(response) {
         console.log("changed info is ", response);
-        $scope.puppy = response;
+        $scope.puppies = response;
       });  
   };
 
